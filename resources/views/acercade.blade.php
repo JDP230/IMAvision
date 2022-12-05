3d406e682717424faa63a48ea2b64c870488cad8
daniel <jd_puente23@hotmail.com>
2022-12-05 09:30:37 -0600

    Cambios en acerca de

--- resources/views/acercade.blade.php
+++ resources/views/acercade.blade.php
@@ -8,21 +8,27 @@
                 <div class="card-body">
                     <div class="header-card">
                         <h1>Acerca de IMAvision!</h1>
-                        <p class="txt-info mt-3">IMAvision es una herramienta ideal para todo interesado en la ingeniería, 
+                        <p class="txt-info mt-3 inter-line13">IMAvision es una herramienta ideal para todo interesado en la ingeniería, 
                             una perfecta guía de aprendizaje a una nueva experiencia académica y laboral. 
                             Únete completamente gratis o inicia sesión y empieza a forjarte como ingeniero a tu medida!</p>
                     </div>
                     <div class="row mt-3">
-                        <span>Integrantes</span>
-                        <span>PROYECT MANAGER: VALERIA PAOLA GONZALES DUEÑES</span>
-                        <span></span>
-                        <span>Juan Daniel Puente Melchor</span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span></span>
-                        <span>ESTE PROTOTIPO ESTÁ</span>
-                        <span>devsupport@imavision.com</span>
+                        <span class="txtN">Integrantes</span>
+                        <span class="txt11 inter-line10">PROYECT MANAGER: VALERIA PAOLA GONZALES DUEÑES</span>
+                        <span class="mt-2 txtN">Equipo de desarrollo</span>
+                        <span class="txt11 inter-line10">Guillermo Adrián Alonso Arámbula </span>
+                        <span class="txt11 inter-line10">Juan Daniel Puente Melchor</span>
+                        <span class="txt11 inter-line10">Lilieth Irasema Minor Delgado</span>
+                        <span class="txt11 inter-line10">Josias Salvador Lona Romero</span>
+                        <span class="txt11 inter-line10">Patricio Humberto Gutierrez Salinas</span>
+                        <span class="txt11 inter-line10">Elena Miroslava Lara Soto</span>
+
+                        <span class="txt10 txt_orange inter-line10 mt-4">
+                            ESTE PROTOTIPO ESTA EN FASE ALPHA, SE DEBE DE CONSIDERAR CAM BIOS PERTINENTES Y 
+                            POSIBLES ERRORES O BUGS QUE INTERRUMPAN LA EXPERIENCIA DE USO, SI OBTIENE ESTE PROBLEMA 
+                            CONTACTE AL SIGUIENTE CORREO CON SU PROBLEMA
+                        </span>
+                        <span class="txt_foro txt11 mt-2">devsupport@imavision.com</span>
 
 
 
